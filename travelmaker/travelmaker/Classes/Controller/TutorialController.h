376b0342791905a4930aaf87b6a7d078bef432c1
filip-chabik <filipchabik@gmail.com>
2015-12-04 14:55:29 +0100

    update tutorial images.

---
+++ travelmaker/travelmaker/Classes/Controller/TutorialController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialController.h
+//  Travel Maker
+//
+//  Created by developer on 12/4/15.
+//  Copyright © 2015 developer. All rights reserved.
+//
+
+#import "SuperViewController.h"
+
+@interface TutorialController : SuperViewController
+
+@end
